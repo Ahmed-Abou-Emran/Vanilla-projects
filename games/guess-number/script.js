d2bce88bb1746c68838cf0157a1f253cba173e71
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-03-26 20:58:45 +0200

    🎉 implementing difficulty levels

--- games/guess-number/script.js
+++ games/guess-number/script.js
@@ -6,22 +6,55 @@ const numberElement = document.querySelector('.number');
 const messageElement = document.querySelector('.message');
 const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
+const betweenElement = document.querySelector('.between');
+const difficultyInputElement = document.querySelector('.difficulty');
+let difficulty = document.querySelector('.difficulty').value;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highscore = 0;
+// Set the range of numbers and number of attempts based on the difficulty level
+let score, range, highscore, secretNumber;
 
-checkBtn.addEventListener('click', checkHandler);
-againBtn.addEventListener('click', againHandler);
+gameStart();
+
+function gameStart() {
+  [score, range] = getScoreAndRange();
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  highscore = 0;
+  scoreElement.textContent = score;
+  highscoreElement.textContent = highscore;
+  betweenElement.textContent = `Between 1 and ${range}`;
+
+  checkBtn.addEventListener('click', checkHandler);
+  againBtn.addEventListener('click', againHandler);
+  difficultyInputElement.addEventListener('change', difficultyHandler);
+}
+
+function difficultyHandler() {
+  [score, range] = getScoreAndRange();
+
+  scoreElement.textContent = score;
+  secretNumber = Math.trunc(Math.random() * range) + 1;
+  betweenElement.textContent = `Between 1 and ${range}`;
+  againHandler();
+}
+
+function getScoreAndRange() {
+  difficulty = document.querySelector('.difficulty').value;
+  if (difficulty === 'easy') {
+    range = 20;
+    score = 10;
+  } else if (difficulty === 'medium') {
+    range = 50;
+    score = 8;
+  } else if (difficulty === 'hard') {
+    range = 100;
+    score = 5;
+  }
+
+  return [score, range];
+}
 
 function againHandler() {
-  score = 20;
-  scoreElement.textContent = score;
-  messageElement.textContent = 'Start guessing...';
-  numberElement.textContent = '?';
-  document.querySelector('body').style.backgroundColor = '#19A7CE';
-  numberElement.style.width = '15rem';
-  document.querySelector('.guess').value = '';
+  resetGame();
 }
 
 function checkHandler() {
@@ -41,14 +74,33 @@ function checkHandler() {
     }
   } else if (guess !== secretNumber) {
     if (score > 1) {
+      console.log(secretNumber);
       messageElement.textContent =
         guess > secretNumber ? 'Too high!' : 'Too low!';
       score--;
       scoreElement.textContent = score;
     } else {
+      console.log(score, highscore);
       messageElement.textContent = 'You lost the game!';
       document.querySelector('body').style.backgroundColor = '#b34747';
       scoreElement.textContent = 0;
+
+      difficultyInputElement.disabled = true;
+      checkBtn.disabled = true;
     }
   }
+}
+
+function resetGame() {
+  [score, range] = getScoreAndRange();
+  scoreElement.textContent = score;
+  betweenElement.textContent = `Between 1 and ${range}`;
+
+  messageElement.textContent = 'Start guessing...';
+  numberElement.textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#19A7CE';
+  numberElement.style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  difficultyInputElement.disabled = false;
+  checkBtn.disabled = false;
 }
